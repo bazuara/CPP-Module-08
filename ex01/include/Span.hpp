1a3c193bb83e6199985bf6f5a0bd03970bed91ef
Bruno Azuara <bruno@azuara.dev>
2025-02-08 12:00:55 +0100

    check code evaluation and lint ex01 hpp

--- ex01/include/Span.hpp
+++ ex01/include/Span.hpp
@@ -6,34 +6,34 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 17:41:20 by bazuara           #+#    #+#             */
-/*   Updated: 2025/01/20 17:57:08 by bazuara          ###   ########.fr       */
+/*   Updated: 2025/02/08 12:00:25 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
-# define SPAN_HPP
+#define SPAN_HPP
 
-# include <vector>
-# include <algorithm>
-# include <exception>
+#include <algorithm>
+#include <exception>
+#include <vector>
 
-class Span
-{
+class Span {
  private:
-    unsigned int n;
-    std::vector<int> v;
-    Span();
- public:
-    Span(unsigned int n);
-    Span(const Span &span);
-    Span &operator=(const Span &span);
-    ~Span();
+  unsigned int n;
+  std::vector<int> v;
+  Span();
 
-    void addNumber(int n);
-    void addNumber(std::vector<int>::iterator begin, 
-                   std::vector<int>::iterator end);
-    int shortestSpan();
-    int longestSpan();
+ public:
+  explicit Span(unsigned int n);
+  Span(const Span &span);
+  Span &operator=(const Span &span);
+  ~Span();
+
+  void addNumber(int n);
+  void addNumber(std::vector<int>::iterator begin,
+                 std::vector<int>::iterator end);
+  int shortestSpan();
+  int longestSpan();
 };
 
-# endif  // SPAN_HPP
+#endif  // SPAN_HPP
