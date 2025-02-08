44821542c2821ccb15950c88b8d6ff86723be9f8
Bruno Azuara <bruno@azuara.dev>
2025-02-08 11:44:16 +0100

    check code evaluation and lint

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,24 +6,23 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 16:49:36 by bazuara           #+#    #+#             */
-/*   Updated: 2025/01/20 17:06:15 by bazuara          ###   ########.fr       */
+/*   Updated: 2025/02/08 11:42:57 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
-# define EASYFIND_HPP
+#define EASYFIND_HPP
 
-# include <iostream>
-# include <algorithm>
-# include <vector>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 template <typename T>
-typename T::iterator easyfind(T &container, int value)
-{
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-    if (it == container.end())
-        throw std::exception();
-    return it;
+typename T::iterator easyfind(T &container, int value) {
+  typename T::iterator it =
+      std::find(container.begin(), container.end(), value);
+  if (it == container.end()) throw std::exception();
+  return it;
 }
 
-#endif // EASYFIND_HPP
+#endif  // EASYFIND_HPP
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,43 +6,40 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 16:53:16 by bazuara           #+#    #+#             */
-/*   Updated: 2025/01/20 17:09:32 by bazuara          ###   ########.fr       */
+/*   Updated: 2025/02/08 11:41:31 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <easyfind.hpp>
 
-int main(void)
-{
-    std::vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
+int main(void) {
+  std::vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  v.push_back(4);
+  v.push_back(5);
 
-    std::vector<int> v2;
-    v2.push_back(6);
-    v2.push_back(7);
-    v2.push_back(8);
-    v2.push_back(9);
-    v2.push_back(10);
+  std::vector<int> v2;
+  v2.push_back(6);
+  v2.push_back(7);
+  v2.push_back(8);
+  v2.push_back(9);
+  v2.push_back(10);
 
-    try {
+  try {
     std::vector<int>::iterator it = easyfind(v, 3);
     std::cout << "Value found: " << *it << std::endl;
-    }
-    catch (std::exception &e) {
-        std::cout << "Value not found" << std::endl;
-    }
+  } catch (std::exception &e) {
+    std::cout << "Value not found" << std::endl;
+  }
 
-    try {
-        std::vector<int>::iterator it = easyfind(v2, 3);
-        std::cout << "Value found: " << *it << std::endl;
-    }
-    catch (std::exception &e) {
-        std::cout << "Value not found" << std::endl;
-    }
+  try {
+    std::vector<int>::iterator it = easyfind(v2, 3);
+    std::cout << "Value found: " << *it << std::endl;
+  } catch (std::exception &e) {
+    std::cout << "Value not found" << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
